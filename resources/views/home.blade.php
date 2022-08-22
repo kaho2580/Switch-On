151f510c4691d468fd18f080e4c13d92e04aea2d
kaho2580 <katsuma8888@gmail.com>
2022-08-23 01:45:54 +0900

    ログイン機能　いれたら死んだ

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,45 +1,23 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-           
-            
-        <title>Switch On</title>
-    </head>
-    <body>
-        <h1>まだ炊いていないのか、早く炊けツ！！</h1>
-        
-        <!-- ここにGoogle Mapを表示する -->
-<div id="map" class="map"></div>
+@extends('layouts.app')
 
-<style>
-.map {
-  width: 400px;
-  height: 400px;
-}
-</style>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
-<!-- APIキーを指定してjsファイルを読み込む -->
-<script async src="https://maps.googleapis.com/maps/api/js?key={{config('app.key_API03')}}&callback=initMap"></script>
-<script type="text/javascript">
-  function initMap(){
-    const myLatLng = { lat: 35.6585805, lng: 139.7454329 };
-    const map=new google.maps.Map(document.getElementById("map"),{
-      zoom: 5,
-      center: myLatLng,
-    });
-  }
-  
-  window.initMap=initMap;
-</script>
-        
-        <p class=posts>
-            <a href='/posts'>炊飯器の準備はできたか。ならばここを押せッ！！</a>
-        </p>
-       
-      <a href='/auto-complete-address'>位置情報検索</a>
-      <a href='/google-autocomplete'>google map api 参考</a>
-       
-    </body>
-</html>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,28 @@ use App\Http\Controllers\GoogleController;
 |
 */
 
+//Route::get('/', function () {
+    //return view('home');
+//});
+
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/auto-complete-address', [AutoAddressController::class, 'index']);
 Route::get('/google-autocomplete', [GoogleController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
